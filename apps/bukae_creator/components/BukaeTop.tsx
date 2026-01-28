cf740f83509b8eb488555c876c30cce8c7c89aa4
atank3566 <atank3566@naver.com>
2026-01-28 20:55:51 +0900

    [creator] design: 헤더 마진 추가

--- apps/bukae_creator/components/BukaeTop.tsx
+++ apps/bukae_creator/components/BukaeTop.tsx
@@ -129,7 +129,7 @@ export default function BukaeTop({
             })
           }}
         >
-          <div className="max-w-[1760px] mx-auto pb-4 flex justify-start" style={{ paddingLeft: '80px', paddingRight: '80px' }}>
+          <div className="max-w-[1760px] mx-auto pb-8 flex justify-start" style={{ paddingLeft: '80px', paddingRight: '80px' }}>
             <StepNavigation
               steps={steps || defaultSteps}
               currentStep={detectedCurrentStep}
